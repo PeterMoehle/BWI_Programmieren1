434b77c312be1d5945090a14155728747b166601
PeterMoehle <moehle.peter@gmail.com>
2024-02-20 20:23:33 +0100

    Übung2

---
+++ BWI_Programmieren1/src/Lektion1/Übung2.java
@@ -0,0 +1,12 @@
+package Lektion1;
+
+// Schreiben Sie ein Programm, welches folgenden Term korrekt berechnet und das 
+// Ergebnis ausgibt: 3 * (9 + 3) + 4 * 8
+
+public class Übung2 {
+
+	public static void main(String[] args) {
+		System.out.println(3 * (9 + 3) + 4 * 8);
+	}
+
+}
