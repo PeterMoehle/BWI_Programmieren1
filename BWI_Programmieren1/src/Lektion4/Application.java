07ea1a4687ca7219e2518391dc700f485dd5a8da
PeterMoehle <moehle.peter@gmail.com>
2024-02-23 07:54:10 +0100

    L4Ü1

---
+++ BWI_Programmieren1/src/Lektion4/Application.java
@@ -0,0 +1,13 @@
+package Lektion4;
+
+public class Application {
+
+	public static void main(String[] args) {
+		try {
+			Übung1.run();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ BWI_Programmieren1/src/Lektion4/Übung1.java
@@ -0,0 +1,20 @@
+package Lektion4;
+
+//a) Schreiben Sie ein Programm, das einen Countdown von 15 bis 0 herunterzählt und 
+//jeweils die verbleibende (Sekunden-)zahl ausgibt.
+//b) (optional) Wenn Sie einen echten Countdown implementieren wollen, müssen Sie 
+//sekündlich herunterzählen. Dies ist durch den Befehl Thread.sleep(millis) möglich. Er 
+//legt das Programm für millis Millisekunden schlafen, bevor die nächste Anweisung 
+//ausgeführt wird, erzwingt aber die Behandlung einer InterruptedException. Sie 
+//können bei dieser Aufgabe einfach die Exception ignorieren, indem Sie hinter dem 
+//Kopf der main-Methode throws InterruptedException ergänzen:
+
+public class Übung1 {
+
+	public static void run() throws InterruptedException {
+		for (int i = 15; i >= 0; i--) {
+			System.out.println(i);
+			Thread.sleep(1000);
+		}
+	}
+}
