5f11fd6f60032ad00115373bfe5a86c036fbcab2
PeterMoehle <moehle.peter@gmail.com>
2024-02-23 08:12:09 +0100

    L4Ü5

--- BWI_Programmieren1/src/Lektion4/Application.java
+++ BWI_Programmieren1/src/Lektion4/Application.java
@@ -5,9 +5,10 @@ public class Application {
 	public static void main(String[] args) {
 		try {
 			Übung1.run();
-			Übung2.run();
-			Übung3.run();
-			Übung4.run();
+//			Übung2.run();
+//			Übung3.run();
+//			Übung4.run();
+			Übung5.run();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
---
+++ BWI_Programmieren1/src/Lektion4/Übung5.java
@@ -0,0 +1,37 @@
+package Lektion4;
+
+//Schreiben Sie ein Programm namens Querprodukt, welches das „Querprodukt“
+//einer int-Zahl berechnet und dieses mit einer Meldung am Bildschirm anzeigt. Das 
+//Einlesen der int-Zahl soll von der Standardeingabe solange wiederholt werden, bis 
+//sie gültig ist, d.h. zwischen 1 und einer Million (jeweils inklusive) liegt. Bei Eingabe 
+//einer ungültigen int-Zahl soll zusätzlich eine Fehlermeldung erscheinen und ein 
+//erneutes Einlesen erfolgen.
+//Beispiel:
+//Geben Sie bitte eine Zahl ein: -3
+//FEHLER – Zahl ist ungueltig.
+//Geben Sie bitte eine Zahl ein: 1348213
+//FEHLER – Zahl ist ungueltig.
+//Geben Sie bitte eine Zahl ein: 3225
+//Das Querprodukt der Zahl 3225 betraegt 60
+
+public class Übung5 {
+	public static void run() {
+		int number = 0;
+		while (true) {
+			System.out.print("Geben Sie bitte eine Zahl ein: ");
+			number = new java.util.Scanner(System.in).nextInt();
+			if (number >= 1 && number <= 1000000) {
+				break;
+			} else {
+				System.out.println("FEHLER – Zahl ist ungueltig.");
+			}
+		}
+		int result = 1;
+		int temp = number;
+		while (temp > 0) {
+			result *= temp % 10;
+			temp /= 10;
+		}
+		System.out.println("Das Querprodukt der Zahl " + number + " betraegt " + result);
+	}
+}
