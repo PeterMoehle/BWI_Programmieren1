bd407c377624273ba6dc5c0c575a58aa0a572ebc
PeterMoehle <moehle.peter@gmail.com>
2024-02-24 19:59:16 +0100

    L5

--- BWI_Programmieren1/src/Lektion5/Application.java
+++ BWI_Programmieren1/src/Lektion5/Application.java
@@ -3,10 +3,10 @@ package Lektion5;
 public class Application {
 
 	public static void main(String[] args) {
-		//Übung1.run();
-		//Übung2.run();
-		//Übung3.run();
-		//Übung4.run();
+		Übung1.run();
+		Übung2.run();
+		//Übung3.run(); Kein Code notwendig
+		Übung4.run();
 		Übung5.run();
 	}
 
