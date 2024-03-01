c80c83e8bdee1b26a78cac661aad217bfeabd4bc
PeterMoehle <moehle.peter@gmail.com>
2024-03-01 10:54:23 +0100

    Lektion8

---
+++ BWI_Programmieren1/src/Lektion8/Application.java
@@ -0,0 +1,13 @@
+package Lektion8;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Übung1.run();
+		Übung2.run(new String[] {"1", "2", "3"});
+		Übung3.run(new String[] {"1", "2", "3"});
+		Übung4.run();
+		Übung5.run();
+	}
+
+}
